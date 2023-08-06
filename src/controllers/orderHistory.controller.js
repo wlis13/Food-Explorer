2550f895ce3a7f870967f1d6d875eee666c56e01
wlisses <wlissesfernando285@gmail.com>
2023-08-06 08:46:17 -0300

    criada função service para atualização de orderHistory

--- src/controllers/orderHistory.controller.js
+++ src/controllers/orderHistory.controller.js
@@ -1,5 +1,24 @@
+const { createOrderHistoryService } = require("../services/orderHistory.service");
+const { getRequestByUserIdService } = require("../services/request.service");
+
 async function getOrderHistoryByUserIdController(req, res) {
   try {
+    const userId = req.params;
+    const getRequest = await getRequestByUserIdService(userId);
+    let newOrderHistoryDetails = [];
+
+    getRequest.map((item) => {
+      newOrderHistoryDetails.push(`${item.amount} X ${item.title}`)
+    });
+
+    let details = newOrderHistoryDetails.join(", ");
+    const createNewObject = {
+      details,
+      userId
+    }
+
+    const getCreatedNewOrder = await createOrderHistoryService(createNewObject);
+    res.status(201).json(getCreatedNewOrder);
 
   } catch (error) {
     res.status(500).json({ message: `Não foi possível encontrar pedido: ${error}` })
--- src/services/orderHistory.service.js
+++ src/services/orderHistory.service.js
@@ -5,6 +5,18 @@ async function getOrderHistoryByUserIdService(id) {
   return getOrder;
 };
 
+async function createOrderHistoryService(newOrder) {
+  const getCreateOrder = await OrderHistory.create(newOrder);
+  return getCreateOrder;
+};
+
+async function updateOrderHistoryService(id, upDateOrder) {
+  const getUpdate = await OrderHistory.updateOne({ where: { _id: id } }, { $set: { upDateOrder } });
+  return getUpdate;
+};
+
 module.exports = {
   getOrderHistoryByUserIdService,
+  createOrderHistoryService,
+  updateOrderHistoryService,
 };
