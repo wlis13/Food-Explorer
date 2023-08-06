2fc110cbe80b04b36e09416a16839e968ba619c9
wlisses <wlissesfernando285@gmail.com>
2023-08-06 09:00:25 -0300

    criadas funções service e controller para fazer a busca por orderHistory através do id do usuário e ordenada pelo status

--- src/controllers/orderHistory.controller.js
+++ src/controllers/orderHistory.controller.js
@@ -1,4 +1,4 @@
-const { createOrderHistoryService } = require("../services/orderHistory.service");
+const { createOrderHistoryService, getOrderByUserIdAndStatus } = require("../services/orderHistory.service");
 const { getRequestByUserIdService } = require("../services/request.service");
 
 async function getOrderHistoryByUserIdController(req, res) {
@@ -21,10 +21,22 @@ async function getOrderHistoryByUserIdController(req, res) {
     res.status(201).json(getCreatedNewOrder);
 
   } catch (error) {
-    res.status(500).json({ message: `Não foi possível encontrar pedido: ${error}` })
+    res.status(500).json({ message: `Erro no servidor: ${error}` })
   }
 };
 
+async function indexOrderHistoryController(req, res) {
+  try {
+    const userId = req.params.id;
+    const getOrderByUserId = await getOrderByUserIdAndStatus(userId);
+    getOrderByUserId.reverse();
+    res.status(200).json(getOrderByUserId);
+  } catch (error) {
+    res.status(500).json({ message: `Erro no servidor: ${error}` })
+  }
+}
+
 module.exports = {
   getOrderHistoryByUserIdController,
+  indexOrderHistoryController,
 };
--- src/services/orderHistory.service.js
+++ src/services/orderHistory.service.js
@@ -5,6 +5,14 @@ async function getOrderHistoryByUserIdService(id) {
   return getOrder;
 };
 
+async function getOrderByUserIdAndStatus(userId) {
+  const getOrder = await OrderHistory.findAll({
+    where:
+      { userId }, order: [['status', 'ASC']]
+  });
+  return getOrder;
+};
+
 async function createOrderHistoryService(newOrder) {
   const getCreateOrder = await OrderHistory.create(newOrder);
   return getCreateOrder;
@@ -17,6 +25,7 @@ async function updateOrderHistoryService(id, upDateOrder) {
 
 module.exports = {
   getOrderHistoryByUserIdService,
+  getOrderByUserIdAndStatus,
   createOrderHistoryService,
   updateOrderHistoryService,
 };
